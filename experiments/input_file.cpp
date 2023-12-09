6bd2c6959e058c6974567b91c1d1d296a8f0bf8f
rockwatch <olivera9806@gmail.com>
2023-12-09 17:02:48 -0400

    [ADD] creating data input function

--- experiments/input_file.cpp
+++ experiments/input_file.cpp
@@ -4,37 +4,124 @@
 
 using namespace std;
 
-int main(){
+int vitalityEntry(){
+    cout << "Insert vitality" << endl;
+    int v = 0;
+    cin >> v;
+    while(v < 1){
+        cout << "invalid vitality: " << endl;
+        cin >> v;
+    }
+    cout << "vitality: " << v << endl;
+    return v;
+} // character vitality
 
+string characterType(){
     char c; //solo un caracter será nuestro elemento de lectura
 
-    cout << "Insert number of rows and columns" << endl;
-    int a, b;
-
-    //columnas y filas
-
-    cin >> a >> b;
-
-    //insertamos columnas y filas. No importan los espacios en blanco
-
-    cout << "rows: " << a << " columns: " << b << endl;
-
-    //mostramos filas y columnas
+    cout << "insert type: " << endl;
+    char type[2];
 
     int i = 0;
-    //elemento iterador, nos dirá hasta cuando iterar por la línea (columnas)
-
-    while(i < b){   // La magia...
-        cin >> c;
-        // este caracter se podrá mover por toda  la fila
-
-        if(c != ' '){ 
-            //si el caracter es vacío no mostramos ni contamos esa entrada como válida (no aumentamos el iterador)
+    while(i < 2){
+        if(c != ' '){
+            cin >> c;
+            type[i] = c;
             i++;
-            cout << c << endl;
-            //mostramos en pantalla lo que escribamos
-        } 
+        }
+    } // loop to insert character type
+    cout << "Type: " << type << endl;
+
+    string t(type, type + sizeof(type));
+    cout << "Type in string: " << t << endl;
+    return t;
+}   // character type entry
+
+int dungeonsQuant(){
+    int q = 0;
+
+    cout << "Insert dungeons quantiity" << endl;
+
+    cin >> q;
+
+    if(q < 1){
+        cout << "Invalid quantity." << endl;
+        cin >> q;
     }
+
+    cout << "dungeons quantity: " << q << endl;
+
+    return q;
+
+} //dungeons quantity entry
+
+bool validator(char c){
+    bool b = false;
+    if(c == '*' || c == '.' || c == 'P' || c == 'E' || c == 'S' || c == 'O' || c == 'G' || c == 'A' || c == 'R'){
+        b = true;
+    }else if(c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9'){
+        b = true;
+    }
+
+    return b;
+}
+
+void dungeonCreator(){
+    int rows, columns = 0;
+
+    cout << "Insert rows and columns" << endl;
+    cin >> rows >> columns; 
+
+    cout << "rows: " << rows << endl;
+    cout << "columns: " << columns << endl;
+
+    char c;
+    char row[c];
+
+    int i = 0;
+
+    while(i < rows){
+        int j = 0;
+        while(j < columns){
+            cin >> c;
+            if(c != ' '){
+                row[j] = c;
+                j++;
+                // if(c == 'P'){
+//  WORK IN PROGRESS...
+                // }
+
+            }
+
+        }
+        cout << row << endl;
+        i++;
+    }
+
+
+}
+
+
+void dataEntry(){
+
+    // vitalityEntry();
+
+    // characterType();
+
+    int q = dungeonsQuant();
+
+    int k = 0;
+    while(k < q){
+
+        dungeonCreator();
+        k++;
+    }
+
+}
+
+int main(){
+
+    dataEntry();
 
     return 0;
 }
